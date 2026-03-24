cd99e620794653f7958385d03f616cc7bff65867
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-03-24 14:46:01 +0800

    alvistack/7.3.0
        git clean -xdf
        tar zcvf ../python-chardet_7.3.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-chardet.spec ../python-chardet_7.3.0-1.spec
        cp ../python*-chardet*7.3.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/chardet-chardet-7.3.0/
        rm -rf ../python*-chardet*7.3.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+setup(
+    name='chardet',
+    version='7.3.0',
+    description='Universal character encoding detector',
+    author_email='Dan Blanchard <dan.blanchard@gmail.com>',
+    maintainer='Ian Cordasco',
+    maintainer_email='Dan Blanchard <dan.blanchard@gmail.com>',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+    entry_points={
+        'console_scripts': [
+            'chardetect = chardet.cli:main',
+        ],
+    },
+    packages=[
+        'chardet',
+        'chardet.models',
+        'chardet.pipeline',
+    ],
+    package_dir={'': 'src'},
+)
